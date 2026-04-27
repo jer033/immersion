b45bb0830ffb7b533296a7a4204c8ec28363f378
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-27 11:52:07 +0800

    Update inquire.php

--- inquire.php
+++ inquire.php
@@ -23,31 +23,41 @@
 <?php
 
 $must_hide = false;
+$name_err = $email_err = $quest_err = false;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["fname"]) || empty($_POST["lname"])) {
     $must_hide = true;
+    $name_err = true;
   } 
 
   if (empty($_POST["email"])) {
     $must_hide = true;;
+    $email_err  = true;
   } 
   
   if (empty($_POST["question"])) {
     $must_hide = true;
+    $quest_err = true;
   } 
 
   if ($must_hide) { 
     ?>
 <section class = "boxed3" id = "problem">
-Unfortunately, we could not process your request because at least one of the following is true:
+Unfortunately, we could not process your request because of the following reason(s):
 
     <ul>
+        <?php if ($name_err) {?>
         <li> Your first or last name was left blank.
+        <?php } ?>
+        <?php if ($email_err) {?>
         <li> Your email was left blank or invalid.
+        <?php } ?>
+        <?php if ($quest_err) {?>
         <li> You did not input a question.
+        <?php } ?>
     </ul>
 
-Please try again. Thank you!
+You may try resubmitting the form through <a href = "contact.html"> this page </a>. Thank you!
 </section>
 
 <?php
@@ -65,7 +75,7 @@ As a reminder, your inquiry was: <br>
 
 <i> Please wait for 2-3 business days for us to respond. </i> <br>
 
-In the meanwhile, feel free to browse the rest of our website. The home page can be accessed at <a href="index.html">Home</a> <br>
+In the meanwhile, feel free to browse the rest of our website. The home page can be accessed at <a href="index.html">Home</a>. <br>
 You may also refer to our <a href="faq.html">Frequently Asked Questions.</a>
 
 </section>
