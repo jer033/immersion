88819d7e7322e0674ed0bb6f6e7d23f7ed56e5f2
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-27 15:50:43 +0800

    Update faq.php

--- faq.php
+++ faq.php
@@ -16,14 +16,13 @@
 <section class="qnaq"> <p> Q: Were you all doing the same thing during the immersion? </p>
 </section>
 <section class="qnaa"> 
-<p> A: No, we were not. We were assigned to different departments, and each person had their own set of tasks. The main responsibilities:
+<p> A: No, we were not. We were assigned to different departments, and each person had their own set of tasks. The main responsibilities were: </p>
 <ul>
     <li>Jerome Austin Te: Create a website that serves as a portfolio for all the skills learned during the immersion. </li>
     <li>Jacob Tyler Tang: Cross-check for discrepancies in student records between two files: a raw data sheet and the master file. </li>
     <li>Johann Gabriel Go: Conduct research on the implementation of standardized assessments, including copyright laws and cultural differences. </li>
     <li>Sophia Dominique Sy: Conceptualize and draw symbols and logos based on a course on the usage of Adobe Illustrator. </li>
 </ul>
-</p> 
 </section>
 
 <section class="qnaq"> <p> Q: Why does Phoenix have many partner companies? </p>
@@ -31,7 +30,7 @@
 <section class="qnaa"> 
 <p> A: Phoenix has many partner companies because each of them have different roles. 
 Despite this, all the companies have a united mission to better the quality of education in the Philippines. These are the companies affiliated with Phoenix: 
-
+</p>
 <ul>
     <li>Phoenix Publishing House and Sibs Publishing House
     <li>Phoenix Digital Solutions
@@ -39,7 +38,7 @@ Despite this, all the companies have a united mission to better the quality of e
     <li>AREYS Logistics and Distribution Corp
     <li>Global Resources for Assessment Curriculum and Evaluation (GRACE), Inc.
 </ul>
-</p> 
+
 </section>
 
 </body>
