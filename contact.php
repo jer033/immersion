74f759463a3408d061f8869430eef6b3a596c2f2
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-27 13:41:13 +0800

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<?php include 'header.php'; ?>
+
+<h2> Contact Information </h2>
+<section class="boxed" style="background-color: #e4ff82">
+<p> Jerome is the developer of this website. For inquiries, please contact him at: </p>
+<p> Email: jante@student.jca.edu.ph </p>
+<p> Phone: (+63) 976 541 7793 </p>
+<p> Office Hours: 8AM - 5PM </p>
+</section>
+
+<h2> Do you have any inquiries? </h2>
+<section class="forms">
+<!-- for github presentation: CHANGE to inquire.html -->
+<form action = "inquire.php" method = "post">
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname"><br>
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email"><br>
+  <label for="question">Your inquiry:</label><br>
+  <textarea id="question" name="question" rows="8" cols="180"></textarea><br>
+  <input type="submit" value="Submit">
+</form>
+</section>
+
+<h2> Rate us! </h2>
+<section class="forms">
+<form action = "rate.php" method = "post">
+    <!-- create a Likert scale -->
+  <label for="nameop">Name (optional):</label>
+  <input type="text" id="nameop" name="nameop"><br>
+  <label for="emailop">Email (optional):</label>
+  <input type="email" id="emailop" name="emailop"><br>
+
+  Please rate your experience from 1-5:<br>
+  <input type="radio" id="exp1" name="r15" value=1 checked>
+  <label for="exp1">1 (worst experience)</label><br>
+  <input type="radio" id="exp2" name="r15" value=2>
+  <label for="exp2">2 </label><br>
+  <input type="radio" id="exp3" name="r15" value=3>
+  <label for="exp3">3 </label><br>
+  <input type="radio" id="exp4" name="r15" value=4>
+  <label for="exp4">4 </label><br>
+  <input type="radio" id="exp5" name="r15" value=5>
+  <label for="exp5">5 (best experience)</label><br>
+
+
+  <label for="reason">Would you like to explain why?:</label><br>
+  <textarea id="reason" name="reason" rows="8" cols="180"></textarea><br>
+  <input type="submit" value="Submit">
+</form>
+</section>
+
+</body>
+</html>
