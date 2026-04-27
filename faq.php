b6eb2400019d6b2081634ee945080510d0bbe3d5
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-27 15:39:39 +0800

    Update faq.php

--- faq.php
+++ faq.php
@@ -13,11 +13,34 @@
   include 'header.php'; 
 ?>
 
-<h2> This part of the website is under construction. </h2>
-<section class="boxed3"> <!--create a question class and CSS stylesheet for that-->
-Lorem ipsum dolor sit amet.
+<section class="qnaq"> <p> Q: Were you all doing the same thing during the immersion? </p>
+</section>
+<section class="qnaa"> 
+<p> A: No, we were not. We were assigned to different departments, and each person had their own set of tasks. The main responsibilities:
+<ul>
+    <li>Jerome Austin Te: Create a website that serves as a portfolio for all the skills learned during the immersion. </li>
+    <li>Jacob Tyler Tang: Cross-check for discrepancies in student records between two files: a raw data sheet and the master file. </li>
+    <li>Johann Gabriel Go: Conduct research on the implementation of standardized assessments, including copyright laws and cultural differences. </li>
+    <li>Sophia Dominique Sy: Conceptualize and draw symbols and logos based on a course on the usage of Adobe Illustrator. </li>
+</ul>
+</p> 
 </section>
 
+<section class="qnaq"> <p> Q: Why does Phoenix have many partner companies? </p>
 </section>
+<section class="qnaa"> 
+<p> A: Phoenix has many partner companies because each of them have different roles. 
+Despite this, all the companies have a united mission to better the quality of education in the Philippines. These are the companies affiliated with Phoenix: 
+
+<ul>
+    <li>Phoenix Publishing House and Sibs Publishing House
+    <li>Phoenix Digital Solutions
+    <li>PPh Digital Media Production Inc.
+    <li>AREYS Logistics and Distribution Corp
+    <li>Global Resources for Assessment Curriculum and Evaluation (GRACE), Inc.
+</ul>
+</p> 
+</section>
+
 </body>
 </html>
