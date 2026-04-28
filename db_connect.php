681c02782c70aedae23f59fb09466f8653429d4c
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-28 08:55:12 +0800

    Revert db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -11,9 +11,6 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-else {
-    echo "<h1>Database Connection Successful!</h1>";
-}
 
 
 //Nothing: no errors
