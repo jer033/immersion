8f892792aff77022c1bb4a6057f1ade2175b5fda
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-28 08:54:17 +0800

    Update db_connect.php (temporary change)

--- db_connect.php
+++ db_connect.php
@@ -11,6 +11,10 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+else {
+    echo "<h1>Database Connection Successful!</h1>";
+}
+
 
 //Nothing: no errors
 //Something: follow it
