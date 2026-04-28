614ef6e9ed56708781abe2d092a0880fbdfb012e
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-28 17:08:40 +0800

    Update leaderboard.php

--- leaderboard.php
+++ leaderboard.php
@@ -18,12 +18,11 @@ $fetched_data = mysqli_query($conn, $retrieve_command);
 </head>
 <body>
     
-<h3> These are the top 25 players with the highest scores in the <a href="memory.php">memory game</a>. </h3>
-<h3> To be eligible for the leaderboard, you must be logged in and you must have saved your score. </h3>
+<h4> These are the top 25 players with the highest scores in the <a href="memory.php">memory game</a>. </h4>
+<h4> To be eligible for the leaderboard, you must be logged in and you must have saved your score. </h4>
 
-<section class="boxed3">
     <h2>Top 25 Players</h2>
-    <table style="border-collapse: collapse; color: #3d1d2d; table-layout: fixed" >
+    <table style="border-collapse: collapse;table-layout: fixed" >
         <thead>
             <tr style="text-align: left; border-bottom: 2px solid #ffb592;">
                 <th>Rank</th>
@@ -37,14 +36,13 @@ $fetched_data = mysqli_query($conn, $retrieve_command);
             while ($row = mysqli_fetch_assoc($fetched_data)) { 
             ?>
                 <tr>
-                    <td style = "color:white;"><?php echo ++$rank; ?></td>
-                    <td style = "color:pink;"><?php echo htmlspecialchars($row['Username']); ?></td>
-                    <td style = "color:cyan;"><?php echo number_format($row['HighScore']); ?></td>
+                    <td style = "color: #563412;font-size:36px;"><?php echo ++$rank; ?></td>
+                    <td style = "color: #8f0606;font-size:36px;"><?php echo htmlspecialchars($row['Username']); ?></td>
+                    <td style = "color: #122456;font-size:36px;"><?php echo number_format($row['HighScore']); ?></td>
                 </tr>
             <?php } ?>
         </tbody>
     </table>
-</section>
 
 </body>
 </html>
