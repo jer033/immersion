e65af37d752dde02c73a6d3cb20885c497142f8a
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-28 17:11:25 +0800

    Update leaderboard.php

--- leaderboard.php
+++ leaderboard.php
@@ -18,16 +18,16 @@ $fetched_data = mysqli_query($conn, $retrieve_command);
 </head>
 <body>
     
-<h4> These are the top 25 players with the highest scores in the <a href="memory.php">memory game</a>. </h4>
-<h4> To be eligible for the leaderboard, you must be logged in and you must have saved your score. </h4>
+<h3> These are the top 25 players with the highest scores in the <a href="memory.php">memory game</a>. </h3>
+<h3> To be eligible for the leaderboard, you must be logged in and you must have saved your score. </h3>
 
-    <h2>Top 25 Players</h2>
-    <table style="border-collapse: collapse;table-layout: fixed" >
+    <h3>Top 25 Players</h3>
+    <table style="border-collapse: collapse;table-layout: fixed;align-self:center" >
         <thead>
             <tr style="text-align: left; border-bottom: 2px solid #ffb592;">
-                <th>Rank</th>
-                <th>Username</th>
-                <th>Score</th>
+                <th style = "font-size:54px;">Rank</th>
+                <th style = "font-size:54px;">Username</th>
+                <th style = "font-size:54px;">Score</th>
             </tr>
         </thead>
         <tbody>
@@ -46,5 +46,4 @@ $fetched_data = mysqli_query($conn, $retrieve_command);
 
 </body>
 </html>
-
 
