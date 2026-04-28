d1dd6fdf6c5a3d6cf7954e40c3c4d5c9ec672d3d
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-28 11:52:01 +0800

    Update regsuccess.php

--- regsuccess.php
+++ regsuccess.php
@@ -3,11 +3,22 @@ include 'db_connect.php';
 
 $username = mysqli_real_escape_string($conn, $_POST['us']);
 $encrypted_password = password_hash($_POST['pw'], PASSWORD_DEFAULT);
+$check_users = "SELECT * FROM Users WHERE Username = '$username'";
+$result = mysqli_query($conn, $check_users);
+$success = true;
 
-$sql = "INSERT INTO Users (Username, EncryptedPassword)
+if (mysqli_num_rows($result) > 0) {
+    header("Location: register.php?error=username_has_been_taken");
+    $success = false;
+    exit();
+  }
+  else {
+    $sql = "INSERT INTO Users (Username, EncryptedPassword)
 VALUES ('$username', '$encrypted_password')";
+  }
 
-if (! mysqli_query($conn, $sql)) {
+if ((! mysqli_query($conn, $sql)) and ($success)) {
+    $success = false;
     echo "Error: " . mysqli_error($conn);
 }
 ?>
@@ -28,9 +39,14 @@ if (! mysqli_query($conn, $sql)) {
 ?>
 
 <section class="boxed3">
+<?php if ($success) { ?>
 <h2>Congratulations, <?php echo htmlspecialchars($username); ?>! </h2>
 <h3>You have successfully registered a new account.</h3>
-<h3>Ypu may now proceed to our <a href="index.php>log-in page</a>.</h3>
+<h3>You may now proceed to our <a href="index.php">log-in page</a>.</h3>
+<?php }
+else { ?>
+<h3>Unfortunately, something went wrong. Please try again later. </h3>
+<?php } ?>
 </section>
 
 </body>
