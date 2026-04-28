4d62673b55efac8df17a435836470ddcfa626897
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-28 13:42:07 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <header>
 <div class="navbar" id = "menubar">
   <a href="index.php">Home</a>
@@ -5,6 +8,12 @@
   <a href="memory.php">Educational Game</a>
   <a href="contact.php">Contact</a>
   <a href="faq.php">FAQ</a>
+  <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) { ?>
+<a href="logout.php">Logout</a>
+  <?php }
+else { ?>
+<a href="login.php">Login</a>
+<?php } ?>
 </div>
 
 <h1><img src = "phoenix.jpg" alt = " " style="float:right;width:250px;height:80px"><?php echo $page_title; ?></h1>
