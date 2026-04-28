48e4d221556bae13c6e727de7a0f2959e1bbe778
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-28 13:29:06 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+$show_password_error = isset($_GET['error']) && $_GET['error'] == 'incorrect_password';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<?php 
+  $page_title = "Login"; 
+  include 'header.php';
+?>
+
+<section class="forms">
+<form action = "loginsuccess.php" method = "post" >
+  <label for="us">Username (required):</label>
+  <input type="text" id="us" name="us">
+
+  <label for="pw">Password (required):</label>
+  <input type="password" id="pw" name="pw">
+
+<p style="visibility: <?php echo $show_password_error ? 'visible' : 'hidden'; ?>; color:red; font-size:16px; margin:1px;"> 
+    <br> Either the username or password entered is incorrect.
+</p>
+
+  <input type="submit" value="Submit">
+</form>
+</section>
+
+</body>
+</html>
---
+++ loginsuccess.php
@@ -0,0 +1,59 @@
+<?php
+include 'db_connect.php';
+
+$username = mysqli_real_escape_string($conn, $_POST['us']);
+$password = $_POST['pw'];
+$fetch_encrypted = "SELECT EncryptedPassword FROM Users WHERE Username = '$username'";
+$encryptedrow = mysqli_query($conn, $fetch_encrypted);
+$success = false;
+
+if ($row = mysqli_fetch_assoc($encryptedrow)) {
+    $encryptedpw = $row['EncryptedPassword'];
+    if (!password_verify($password, $encryptedpw)) {
+    header("Location: login.php?error=incorrect_password");
+    $success = false;
+    exit();
+  }
+else {
+    $success = true;
+    session_start();
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+}
+}
+else {
+    header("Location: login.php?error=incorrect_password");
+    $success = false;
+    exit();
+}
+
+?>
+
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php 
+  $page_title = "Login Successful"; 
+  include 'header.php';
+?>
+
+<section class="boxed3">
+<?php if ($success) { ?>
+<h2>Welcome back, <?php echo htmlspecialchars($username); ?>! </h2>
+<h3>You have successfully logged in.</h3>
+<h3>You may continue to browse our <a href="index.php">home page</a> or play our <a href="memory.php">game</a> and save your high scores.</h3>
+<?php }
+else { ?>
+<h3>Unfortunately, something went wrong. Please try again later. </h3>
+<?php } ?>
+</section>
+
+</body>
+</html>
