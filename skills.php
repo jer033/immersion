ba618003138b56b9f0d1ca07a66fd34295472809
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-29 11:40:39 +0800

    Update skills.php

--- skills.php
+++ skills.php
@@ -18,33 +18,34 @@
 <!-- <h2> Programming Languages</h2> -->
 <section class="boxed2">
     <h3 style="text-align:right;">HTML</h3>
-    <img src = "html5.png" alt = "HTML" style="float:right;width:100px;height:100px;">
+    <img src = "images/html5.png" alt = "HTML" style="float:right;width:100px;height:100px;">
     <p> HTML ("Hyper Text Markup Language") is the standard markup language for creating Web pages.
 It describes the structure of a Web page through a series of elements that label pieces of content
-and tell the browser how to display the content.
+and tell the browser how to display the content. For example, this entire paragraph is enclosed in ```<p> </p>``` for proper rendering.
 </p>
     <h3>CSS</h3>
-    <img src = "css3.png" alt = "CSS" style="float:left;width:150px;height:150px;">
+    <img src = "images/css3.png" alt = "CSS" style="float:left;width:150px;height:150px;">
     <p style="text-align:left;"> CSS ("Cascading Style Sheets") is the language we use to style a Web page.
 It describes how HTML elements are to be displayed on screen, paper, or in other media. 
-It saves a lot of work in external stylesheets that can control the layout of multiple web pages all at once.
+It saves a lot of work in external stylesheets that can control the layout of multiple web pages all at once. For example, the box wherein this text is contained
+uses a class that is referenced by a stylesheet.
  </p>
     <h3 style="text-align:right;">PHP</h3>
-    <img src = "php.jpg" alt = "PHP" style="float:right;width:250px;height:100px;">
+    <img src = "images/php.jpg" alt = "PHP" style="float:right;width:250px;height:100px;">
     <p> PHP ("PHP: Hypertext Preprocessor") is a widely-used, open source server-side scripting language that executes scripts on the server.
         It is free to download and is used for a variety of purposes, including generating dynamic page content
 processing and modifying files on the server, collecting form data,
-sending and receiving cookies, and analyzing and encrypting data in the database.
+sending and receiving cookies, and analyzing and encrypting data in the database. For example, the forms on the <a href="contact.php">contact</a> page use PHP.
  </p>
 <h3 style="text-align:left;">JavaScript</h3>
-    <img src = "js.png" alt = "JS" style="float:left;width:250px;height:150px;margin-bottom:100px">
+    <img src = "images/js.png" alt = "JS" style="float:left;width:250px;height:150px;margin-bottom:100px">
     <p> JS (JavaScript) is the programming language of the web. It can calculate, manipulate and validate data.
 It can update and change both HTML and CSS. As such, it can allow for a user to interact with a website on their session. For example, it is the technology behind the 
 <a href = "memory.php">memory game</a>.
  </p>
 
 <h3 style="text-align:right;">MySQL</h3>
-    <img src = "mysql.png" alt = "MySQL" style="float:right;width:300px;height:150px;margin-bottom:100px">
+    <img src = "images/mysql.png" alt = "MySQL" style="float:right;width:300px;height:150px;margin-bottom:100px">
     <p> MySQL (My Structured Query Language) is an open source relational database management system (RDBMS). 
         It is used to store data behind the scenes of a website. This allows for form submissions to be processed and can even allow for user interactions through a website!
  </p>
