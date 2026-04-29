3feb3e2974b5783fa0bf69dd5813e30c5ed48451
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-29 15:29:44 +0800

    Add files via upload

---
+++ qotd.php
@@ -0,0 +1,59 @@
+<?php 
+  $page_title = "Question of the Day (QOTD)";
+  include 'header.php'; 
+
+  $show_double_vote_warning = isset($_GET['error']) && $_GET['error'] == 'doublevote';
+  if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['email']) && isset($_POST['ppizza'])) {
+        $jcaemail = mysqli_real_escape_string($conn, $_POST['email']);
+        $count_query = "SELECT COUNT(Email) FROM Pizza WHERE Email = '$jcaemail'";
+        $result = mysqli_query($conn, $count_query);
+        if ($result > 0) {
+            header("Location: qotd.php?error=doublevote");
+            exit();
+        } else {
+            $insert_sql = "INSERT INTO Pizza (Email, Vote)
+            VALUES ('$jcaemail', '$_POST['ppizza']');"
+            mysqli_query($conn, $insert_sql);
+        }
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h2> JUBILEANS, SETTLE A POPULAR DEBATE!!! </h2>
+<h3> THIS IS A SPECIAL EDITION OF THE WEBSITE AVAILABLE ONLY ON: April 30, 2026 </h3>
+<h3> YOU MAY ONLY VOTE ONCE </h3>
+
+<section class="forms">
+<form action = "qotd.php" method = "post">
+  <label for="email">JCA Email (required):</label>
+  <input type="email" id="email" name="email" required pattern="[a-z]+@(jca\.edu\.ph|student\.jca\.edu\.ph)"
+  title="Please use your @jca.edu.ph or @student.jca.edu.ph email address.>
+  <fieldset style="border: none; padding: 0; margin-top: 10px;">
+      <legend>Does pineapple belong on pizza?</legend>
+  <label for="yesppizza">YES</label>
+  <input type="radio" id="yesppizza" name="ppizza" value="Yes" checked> <br>
+  <label for="noppizza">NO</label>
+  <input type="radio" id="noppizza" name="ppizza" value="No">
+  </fieldset>
+  <input type="submit" value="Submit">
+</form>
+</section>
+
+<h3> Note: You do not need to be logged in to vote. </h3>
+
+<?php if ($show_double_vote_warning) { ?>
+<h3 style="color:red"> YOU HAVE ALREADY VOTED. YOU CANNOT VOTE AGAIN! </h3>
+<?php } ?>
+
+</body>
+</html>
