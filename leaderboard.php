f6635c6190cdd2b5d74dec2cf66051bbf21e47f8
jer033 <139696491+jer033@users.noreply.github.com>
2026-04-29 08:22:37 +0800

    Update leaderboard.php

--- leaderboard.php
+++ leaderboard.php
@@ -18,16 +18,16 @@ $fetched_data = mysqli_query($conn, $retrieve_command);
 </head>
 <body>
     
-<h4> These are the top 25 players with the highest scores in the <a href="memory.php">memory game</a>. </h4>
-<h4> To be eligible for the leaderboard, you must be logged in and you must have saved your score. </h4>
+<h4 style="margin-top:3px;margin-bottom:3px;"> These are the top 25 players with the highest scores in the <a href="memory.php">memory game</a>. </h4>
+<h4 style="margin-top:3px;margin-bottom:3px;"> To be eligible for the leaderboard, you must be logged in and you must have saved your score. </h4>
 
     <h3 style="color: rgb(8, 151, 0);">Top 25 Players</h3>
-    <table style="border-collapse: collapse;table-layout: fixed;align-self:center" >
+    <table style="border-collapse: collapse;table-layout:fixed;" >
         <thead>
             <tr style="text-align: left; border-bottom: 2px solid #ffb592;">
-                <th style = "font-size:54px;">Rank</th>
-                <th style = "font-size:54px;">Username</th>
-                <th style = "font-size:54px;">Score</th>
+                <th style = "font-size:36px;">   Rank   </th>
+                <th style = "font-size:36px;">                Username                     </th>
+                <th style = "font-size:36px;">   Score   </th>
             </tr>
         </thead>
         <tbody>
@@ -36,9 +36,9 @@ $fetched_data = mysqli_query($conn, $retrieve_command);
             while ($row = mysqli_fetch_assoc($fetched_data)) { 
             ?>
                 <tr>
-                    <td style = "color: #563412;font-size:36px;"><?php echo ++$rank; ?></td>
-                    <td style = "color: #8f0606;font-size:36px;"><?php echo htmlspecialchars($row['Username']); ?></td>
-                    <td style = "color: #122456;font-size:36px;"><?php echo number_format($row['HighScore']); ?></td>
+                    <td style = "color: #563412;font-size:24px;"><?php echo ++$rank; ?></td>
+                    <td style = "color: #8f0606;font-size:24px;"><?php echo htmlspecialchars($row['Username']); ?></td>
+                    <td style = "color: #122456;font-size:24px;"><?php echo number_format($row['HighScore']); ?></td>
                 </tr>
             <?php } ?>
         </tbody>
@@ -46,5 +46,6 @@ $fetched_data = mysqli_query($conn, $retrieve_command);
 
 </body>
 </html>
+
 
 
